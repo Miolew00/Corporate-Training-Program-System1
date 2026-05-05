6b369953331a4b84dcd814d6890e35e96a576f95
synzt <synzt@KOLEWPC>
2026-05-06 02:53:00 +0300

    Add project files.

---
+++ Corporate-Training-Program-System/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Corporate_Training_Program_System.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
